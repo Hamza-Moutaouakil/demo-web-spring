b94e85b3798cb7ef0ac40da8f86231b4af27bf9f
hamza-moutaouakil <moutaouakilhamza1@gmail.com>
2020-08-11 15:48:16 +0100

    Initial commit

---
+++ src/test/java/com/docker/demoweb/SpringAppDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.demoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
